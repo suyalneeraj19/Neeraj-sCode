3bfaee5313679e312a56e03e8e39faa0b1f0a49a
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-04-19 19:39:04 +0530

    Time: 3 ms (86.63%), Space: 49.3 MB (57.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,22 +7,24 @@ class Solution {
         for(int row=0;row<grid.length;row++){
             for(int col =0;col<grid[0].length;col++){
                 if(grid[row][col]=='1'){
-                    helper(grid,row,col);
-                    count++;
+                    count+=helper(grid,row,col);
+                    
                 }
             }
         }
         return count;
     }
     
-    private static void helper(char [][] grid,int row,int col){
-        if(row <0 || row>=grid.length || col<0||col >=grid[0].length|| grid[row][col]!='1') return;
+    private static int helper(char [][] grid,int row,int col){
+        if(row <0 || row>=grid.length || col<0||col >=grid[0].length|| grid[row][col]!='1') return 0;
         
         grid[row][col]='0';
         helper(grid,row+1,col);
         helper(grid,row-1,col);
         helper(grid,row,col+1);
         helper(grid,row,col-1);
+        
+        return 1;
         
     }
 }
