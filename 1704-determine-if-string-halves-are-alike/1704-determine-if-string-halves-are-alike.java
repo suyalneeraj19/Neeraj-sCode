c7543e3fedb93d1db43a6d93ced0730d820b092b
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-04-27 20:37:29 +0530

    Time: 3 ms (89.17%), Space: 41.9 MB (28.01%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int len=s.length()/2;
-        int c1=0,c2=0;
-        for(int i=0;i<len;i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='A'||s.charAt(i)=='e'||s.charAt(i)=='E'||s.charAt(i)=='i'||s.charAt(i)=='I'||s.charAt(i)=='o'||s.charAt(i)=='O'||s.charAt(i)=='u'||s.charAt(i)=='U'){
-                c1++;
-            }
+        int len = s.length();
+    int count1 = 0, count2 = 0;
+    String vowels = "aAeEiIoOuU";
+
+    for (int i = 0; i < len / 2; i++) {
+        if (vowels.indexOf(s.charAt(i)) != -1) {
+            count1++;
         }
-        
-        for(int i=len;i<s.length();i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='A'||s.charAt(i)=='e'||s.charAt(i)=='E'||s.charAt(i)=='i'||s.charAt(i)=='I'||s.charAt(i)=='o'||s.charAt(i)=='O'||s.charAt(i)=='u'||s.charAt(i)=='U'){
-                c2++;
-            }
+        if (vowels.indexOf(s.charAt(i + len / 2)) != -1) {
+            count2++;
         }
-        
-        return (c1==c2);
+    }
+
+    return count1 == count2;
     }
 }
