88d9f286039a94f7fa23141e9bce7f929b4d5a16
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-05-03 19:45:15 +0530

    Time: 0 ms (100.00%), Space: 40 MB (83.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,5 @@
 class Solution {
     public int fib(int n) {
-        if(n<2){
-            return n;
-        }
-        
-        return fib(n-2)+fib(n-1);
+        return (int) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
 }
