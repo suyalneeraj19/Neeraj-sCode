d9e422374e2c4ae84a64c7f55692c3b5ad163d71
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-06-05 16:37:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (99.99%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,39 @@
 class Solution {
-    public int[] searchRange(int[] arr, int t) {
-        int []ans={-1,-1};
-        
-        ans[0]=bs(arr,t,true);
-        if(ans[0]!=-1){
-            ans[1]=bs(arr,t,false);
-        }
-        
-        return ans;
-    }
-    
-    private static int bs(int [] arr ,int t,boolean isFirst){
-        int s=0;
-        int e=arr.length-1;
-        int ans=-1;
-        
-        
-        
-        while(s<=e){
-            int m=s+(e-s)/2;
-            if(t<arr[m]){
-                e=m-1;
-            }else if(t>arr[m]){
-                s=m+1;
-            }else{
-                ans=m;
-                if(isFirst){
-                    e=m-1;
-                }else{
-                    s=m+1;
-                }
-            }
-        }
-        
-        return ans;
-    }
+   public int[] searchRange(int[] nums, int target) {
+		 
+		 	int[] ans= {-1,-1};
+			ans[0]=search(nums,target,true);
+			if(ans[0]!=-1){
+				ans[1]=search(nums,target,false);
+			}
+			return ans;
+	    }
+	 
+	 int search(int[] nums,int target,boolean findstartindex) {
+		 	int ans=-1;
+		 	int start=0;
+			int end=nums.length-1;
+			
+			
+			while(start<=end) {
+				int mid=start+(end-start)/2;
+				if(target<nums[mid]) {
+					end=mid-1;
+				}
+				else if(target>nums[mid]) {
+					start=mid+1;
+				}
+				else {
+					ans= mid;
+					if(findstartindex) {
+						end=mid-1;
+					}
+					else {
+						start=mid+1;
+					}
+				}
+			}
+			return ans;
+
+	 }
 }
