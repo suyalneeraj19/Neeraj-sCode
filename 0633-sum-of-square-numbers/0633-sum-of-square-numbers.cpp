ae3a53759983e8580099b11958a7d650805d4eed
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-06-17 06:52:08 +0530

    Time: 0 ms (100%), Space: 7.1 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,20 @@
+
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long s = 0, e = (long) std::sqrt(c);
+        while (s <= e) {
+            long sum = s * s + e * e;
+            if (sum < c) {
+                s++;
+            } else if (sum > c) {
+                e--;
+            } else {
+                return true;
+            }
+        }
+        return c == 1;
+    }
+};
+
