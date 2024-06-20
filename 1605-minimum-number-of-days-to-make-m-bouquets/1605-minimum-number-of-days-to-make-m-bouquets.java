7ffbf0d07fd2b5878f84e26512e6afba7dfeb13a
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-06-20 05:33:29 +0530

    Time: 17 ms (83%), Space: 57.7 MB (63.66%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,42 +1,39 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if (bloomDay.length < m * k) {
+        if ((long) m * k > bloomDay.length) {
             return -1;
-        }
-        int low = 1, high = 1, n = bloomDay.length, ans = -1;
-        if (m > (n / k)) {
-            return -1;
-        }
-        for (int i = 0; i < bloomDay.length; i++) {
-            high = Math.max(high, bloomDay[i]);
         }
 
-        while (low <= high) {
+        int low = 1, high = (int) 1e9;
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if (canHappen(mid, bloomDay, m, k)) {
-                ans = mid;
-                high = mid - 1;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
             } else {
                 low = mid + 1;
             }
         }
 
-        return ans;
+        return low;
     }
 
-    private boolean canHappen(int days, int[] bloomDay, int m, int k) {
-        int K = 0, bouquets = 0;
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
+        int count = 0;
+
         for (int i = 0; i < bloomDay.length; i++) {
-            if (bloomDay[i] <= days) {
-                K++;
-                if (K == k) {
-                    bouquets++;
-                    K = 0;
+            if (bloomDay[i] <= day) {
+                count++;
+                if (count == k) {
+                    total++;
+                    count = 0;
                 }
             } else {
-                K = 0;
+                count = 0;
             }
         }
-        return bouquets >= m;
+
+        return total >= m;
     }
-};
+}
