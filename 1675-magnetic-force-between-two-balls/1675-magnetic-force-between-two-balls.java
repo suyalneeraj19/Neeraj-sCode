0efb84a94a49df582addbaae1e332782671af7a9
Neeraj Suyal <148360932+suyalneeraj19@users.noreply.github.com>
2024-06-20 05:36:16 +0530

    Time: 40 ms (92.6%), Space: 55.8 MB (37.12%) - LeetHub

---
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean fn(int[] arr, int m, int mid) {
+        int cnt = 1, prev = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - prev >= mid) {
+                cnt++;
+                prev = arr[i];
+            }
+        }
+        return cnt >= m;
+    }
+
+    public int maxDistance(int[] p, int m) {
+        Arrays.sort(p);
+        int lo = 1, hi = p[p.length - 1] - p[0];
+        while (hi - lo > 1) {
+            int mid = (lo + hi) / 2;
+            if (fn(p, m, mid)) lo = mid;
+            else hi = mid - 1;
+        }
+        if (fn(p, m, hi)) return hi;
+        return lo;
+    }
+}
