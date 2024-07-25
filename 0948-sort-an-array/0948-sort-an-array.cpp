5848b3844b0cba41ef7bc55162e3ab6b8d98c47c
Neeraj Suyal <suyalneeraj09@gmail.com>
2024-07-25 19:44:00 +0530

    Time: 104 ms (76.04%), Space: 69.3 MB (86.44%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,45 +1,38 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
+    void heapify(vector<int>& nums, int i, int n) {
+        int largest = i; // Change smallest to largest for max-heap
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && nums[left] > nums[largest]) {
+            largest = left;
+        }
+        if (right < n && nums[right] > nums[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            swap(nums[largest], nums[i]);
+            heapify(nums, largest, n);
+        }
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size());
+        int n = nums.size();
+        // Build max heap
+        for (int i = (n / 2) - 1; i >= 0; i--) {
+            heapify(nums, i, n);
+        }
+        // Extract elements from the heap
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums[0], nums[i]); // Move current root to end
+            heapify(nums, 0, i); // Heapify the reduced heap
+        }
         return nums;
-    }
-
-private:
-    void mergeSort(vector<int>& arr, int s, int e) {
-        if (e - s <= 1) {
-            return;
-        }
-
-        int m = s + (e - s) / 2;
-
-        mergeSort(arr, s, m);
-        mergeSort(arr, m, e);
-
-        merge(arr, s, m, e);
-    }
-
-    void merge(vector<int>& arr, int s, int m, int e) {
-        int i = s, j = m, k = 0;
-        vector<int> mix(e - s);
-
-        while (i < m && j < e) {
-            if (arr[i] < arr[j]) {
-                mix[k++] = arr[i++];
-            } else {
-                mix[k++] = arr[j++];
-            }
-        }
-
-        while (i < m) {
-            mix[k++] = arr[i++];
-        }
-        while (j < e) {
-            mix[k++] = arr[j++];
-        }
-
-        for (int l = 0; l < mix.size(); l++) {
-            arr[s + l] = mix[l];
-        }
     }
 };
