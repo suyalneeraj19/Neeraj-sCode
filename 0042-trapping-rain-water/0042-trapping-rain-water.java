66ff526952c7eabb123d9ce7acc365d19cdbef1d
Neeraj Suyal <suyalneeraj09@gmail.com>
2025-07-26 21:09:06 +0530

    Time: 0 ms (100%), Space: 46.8 MB (13.3%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,24 @@
 class Solution {
     public int trap(int[] A) {
-        int s = 0 , e = A.length - 1 , sMax = A[s] , eMax = A[e] ,tot = 0;
+        int s = 0 , e = A.length - 1 , tot = 0 , sMax = A[s] , eMax = A[e]; 
 
         while(s < e){
-            if(A[s] < A[e]){
+           if(A[s] < A[e] ){
                 sMax = Math.max(sMax , A[s]);
                 if(sMax - A[s] > 0){
                     tot += sMax - A[s];
                 }
                 s++;
-            }else{
+           }else{
                 eMax = Math.max(eMax , A[e]);
                 if(eMax - A[e] > 0){
                     tot += eMax - A[e];
                 }
                 e--;
-            }
+           }
         }
 
         return tot;
     }
 }
+
